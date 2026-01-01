b15b08e50a4d03a63abb0373936a05d1ad1470a4
Matheshraju <101191899+Matheshraju4@users.noreply.github.com>
2026-01-01 22:45:19 +0530

    Update dependencies and integrate TRPC with React Query in layout and page components

---
+++ src/trpc/routers/_app.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+import { baseProcedure, createTRPCRouter } from "../init";
+import prisma from "@/lib/db";
+export const appRouter = createTRPCRouter({
+  hello: baseProcedure.query(async () => {
+    return await prisma.user.findMany();
+  }),
+});
+// export type definition of API
+export type AppRouter = typeof appRouter;
