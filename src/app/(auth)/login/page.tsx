6c168e700511d7ab86a8d2424c5b972afa9f664a
Matheshraju <101191899+Matheshraju4@users.noreply.github.com>
2026-01-04 12:34:26 +0530

    Enhance authentication forms with image icons and update global styles for improved design consistency

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,11 +1,14 @@
 import { LoginForm } from "@/features/auth/components/login-form"
 import { requireUnAuth } from "@/lib/auth-utils"
+import Image from "next/image"
+import Link from "next/link"
 
 const Page = async () => {
 
     await requireUnAuth()
 
     return <LoginForm />
+
 }
 
 export default Page
