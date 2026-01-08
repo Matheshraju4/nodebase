fca4d3f7e8782b5c4b020957ea8422d296b94766
Matheshraju <101191899+Matheshraju4@users.noreply.github.com>
2026-01-08 20:50:16 +0530

    11: Workflows crud

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -7,33 +7,9 @@ import { google } from "@ai-sdk/google";
 import { generateText } from "ai";
 
 import * as Sentry from "@sentry/nextjs";
+import { workflowsRouter } from "@/features/workflows/server/routers";
 export const appRouter = createTRPCRouter({
-  testAi: protectedProcedure.mutation(async () => {
-    Sentry.logger.info("User triggered test log", {
-      log_source: "sentry_test",
-    });
-
-    console.warn("This is Log by me");
-    await inngest.send({
-      name: "execute/ai",
-    });
-    return { success: true, message: "Job Queued" };
-  }),
-
-  getWorkflows: protectedProcedure.query(async ({ ctx }) => {
-    return await prisma.workflow.findMany({});
-  }),
-
-  createWorkflow: protectedProcedure.mutation(async () => {
-    await inngest.send({
-      name: "test/hello.world",
-      data: {
-        email: "matheshraju@gmail.com",
-      },
-    });
-
-    return { success: true, message: "Job Queued" };
-  }),
+  workflows: workflowsRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
