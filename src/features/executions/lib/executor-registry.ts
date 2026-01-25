1a3b9dc6e1a924e44a58d5594f4745504e929881
Matheshraju <101191899+Matheshraju4@users.noreply.github.com>
2026-01-25 22:11:46 +0530

    23:Stripe Trigger

--- src/features/executions/lib/executor-registry.ts
+++ src/features/executions/lib/executor-registry.ts
@@ -1,17 +1,19 @@
 import { googleFormTriggerExecutor } from "@/features/triggers/components/google-form-trigger/executor";
 import { manualTriggerExecutor } from "@/features/triggers/components/manual-trigger/executor";
+import { stripeTriggerExecutor } from "@/features/triggers/components/stripe-trigger/executor";
 import { NodeType } from "@/generated/prisma/enums";
 import { HttpRequestExecutor } from "../components/http-request/executor";
 import { NodeExecutor } from "../types";
 
-export const executorRegistry: Record<NodeType, NodeExecutor<any>> = {
+export const executorRegistry: Record<NodeType, NodeExecutor> = {
   [NodeType.MANUAL_TRIGGER]: manualTriggerExecutor,
   [NodeType.HTTP_REQUEST]: HttpRequestExecutor,
   [NodeType.INITIAL]: manualTriggerExecutor,
   [NodeType.GOOGLE_FORM_TRIGGER]: googleFormTriggerExecutor,
+  [NodeType.STRIPE_TRIGGER]: stripeTriggerExecutor,
 };
 
-export const getExecutor = (type: NodeType): NodeExecutor<any> => {
+export const getExecutor = (type: NodeType): NodeExecutor => {
   const executor = executorRegistry[type];
   if (!executor) {
     throw new Error(`No executor found for node type: ${type}`);
