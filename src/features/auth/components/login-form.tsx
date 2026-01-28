e33f10231662a06f845b2ea3beb1f70bb650443b
Matheshraju <101191899+Matheshraju4@users.noreply.github.com>
2026-01-28 23:05:20 +0530

    29:Github,Google Auth

--- src/features/auth/components/login-form.tsx
+++ src/features/auth/components/login-form.tsx
@@ -1,130 +1,201 @@
-"use client"
-import { useForm } from "react-hook-form";
-import z, { email } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod"
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import Link from "next/link";
-import { authClient } from "@/lib/auth-client";
-import { toast } from "sonner";
-import { useRouter } from "next/navigation";
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
 import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import z, { email } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client";
 
 const LoginFormSchema = z.object({
-    email: z.email("Enter valid Email"),
-    password: z.string().min(1, "Password is Required")
-})
+  email: z.email("Enter valid Email"),
+  password: z.string().min(1, "Password is Required"),
+});
 
-
-type LoginFormValues = z.infer<typeof LoginFormSchema>
+type LoginFormValues = z.infer<typeof LoginFormSchema>;
 export function LoginForm() {
+  const router = useRouter();
 
-    const router = useRouter()
+  const form = useForm<LoginFormValues>({
+    resolver: zodResolver(LoginFormSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
 
-    const form = useForm<LoginFormValues>({
-        resolver: zodResolver(LoginFormSchema),
-        defaultValues: {
-            email: "",
-            password: ""
-        }
-    })
+  const signInGithub = async () => {
+    await authClient.signIn.social(
+      {
+        provider: "github",
+      },
+      {
+        onSuccess: () => {
+          router.push("/");
+        },
+        onError: () => {
+          toast.error("Something went wrong");
+        },
+      },
+    );
+  };
 
+  const signInGoogle = async () => {
+    await authClient.signIn.social(
+      {
+        provider: "google",
+      },
+      {
+        onSuccess: () => {
+          router.push("/");
+        },
+        onError: () => {
+          toast.error("Something went wrong");
+        },
+      },
+    );
+  };
 
-    const onSubmit = async (values: LoginFormValues) => {
-        await authClient.signIn.email({
-            email: values.email,
-            password: values.password,
-            callbackURL: "/"
-        }, {
-            onSuccess: () => {
-                router.push("/")
-            },
-            onError(context) {
-                toast.error(context.error.message)
-            },
-        })
-    }
+  const onSubmit = async (values: LoginFormValues) => {
+    await authClient.signIn.email(
+      {
+        email: values.email,
+        password: values.password,
+        callbackURL: "/",
+      },
+      {
+        onSuccess: () => {
+          router.push("/");
+        },
+        onError(context) {
+          toast.error(context.error.message);
+        },
+      },
+    );
+  };
 
+  const isPending = form.formState.isSubmitting;
 
-    const isPending = form.formState.isSubmitting
+  return (
+    <div className="flex flex-col gap-6">
+      <Card>
+        <CardHeader className="text-center">
+          <CardTitle>Welcome back</CardTitle>
 
-    return (
-        <div className="flex flex-col gap-6">
-            <Card>
-                <CardHeader className="text-center">
-                    <CardTitle>
-                        Welcome back
+          <CardDescription>Login to Continue</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <div className="grid gap-6">
+                <div className="flex flex-col gap-4">
+                  <Button
+                    onClick={signInGithub}
+                    variant={"outline"}
+                    className="w-full "
+                    type="button"
+                    disabled={isPending}
+                  >
+                    <Image
+                      src="/logos/github.svg"
+                      alt="Github"
+                      width={20}
+                      height={20}
+                    />{" "}
+                    Continue with Github
+                  </Button>
+                  <Button
+                    onClick={signInGoogle}
+                    variant={"outline"}
+                    className="w-full "
+                    type="button"
+                    disabled={isPending}
+                  >
+                    <Image
+                      src="/logos/google.svg"
+                      alt="Google"
+                      width={20}
+                      height={20}
+                    />{" "}
+                    Continue with Google
+                  </Button>
+                </div>
+                <div className="grid gap-6">
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => {
+                      return (
+                        <FormItem>
+                          <FormLabel>Email</FormLabel>
+                          <FormControl>
+                            <Input
+                              type="email"
+                              placeholder="m@example.com"
+                              {...field}
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      );
+                    }}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => {
+                      return (
+                        <FormItem>
+                          <FormLabel>Password</FormLabel>
+                          <FormControl>
+                            <Input
+                              type="password"
+                              placeholder="********"
+                              {...field}
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      );
+                    }}
+                  />
+                  <Button type="submit" className="w-full" disabled={isPending}>
+                    Login
+                  </Button>
+                </div>
 
-                    </CardTitle>
-
-                    <CardDescription>
-                        Login to Continue
-                    </CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <Form {...form}>
-                        <form onSubmit={form.handleSubmit(onSubmit)}>
-                            <div className="grid gap-6">
-
-                                <div className="flex flex-col gap-4">
-
-                                    <Button variant={"outline"} className="w-full " type="button" disabled={isPending}>
-                                        <Image src="/logos/github.svg" alt="Github" width={20} height={20} />  Continue with Github
-                                    </Button>
-                                    <Button variant={"outline"} className="w-full " type="button" disabled={isPending}>
-                                        <Image src="/logos/google.svg" alt="Github" width={20} height={20} />  Continue with Google
-                                    </Button>
-                                </div>
-                                <div className="grid gap-6">
-                                    <FormField
-                                        control={form.control}
-                                        name="email"
-                                        render={({ field }) => {
-                                            return <FormItem>
-                                                <FormLabel>
-                                                    Email
-                                                </FormLabel>
-                                                <FormControl>
-                                                    <Input type="email" placeholder="m@example.com" {...field} />
-                                                </FormControl>
-                                                <FormMessage />
-                                            </FormItem>
-                                        }}
-                                    />
-                                    <FormField
-                                        control={form.control}
-                                        name="password"
-                                        render={({ field }) => {
-                                            return <FormItem>
-                                                <FormLabel>
-                                                    Password
-                                                </FormLabel>
-                                                <FormControl>
-                                                    <Input type="password" placeholder="********" {...field} />
-                                                </FormControl>
-                                                <FormMessage />
-                                            </FormItem>
-                                        }}
-                                    />
-                                    <Button type="submit" className="w-full" disabled={isPending}>
-                                        Login
-                                    </Button>
-                                </div>
-
-                                <div className="text-center text-sm">Don't have an Account?
-
-                                    <Link href={"/signup"} className="underline underline-offset-4">Sign Up</Link>
-                                </div>
-                            </div>
-                        </form>
-                    </Form>
-                </CardContent>
-            </Card>
-
-        </div>
-    )
-
-
+                <div className="text-center text-sm">
+                  Don't have an Account?
+                  <Link
+                    href={"/signup"}
+                    className="underline underline-offset-4"
+                  >
+                    Sign Up
+                  </Link>
+                </div>
+              </div>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,9 @@
+import { checkout, polar, portal } from "@polar-sh/better-auth";
+import { Polar } from "@polar-sh/sdk";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "./db";
-import { Polar } from "@polar-sh/sdk";
 import { polarClient } from "./polar";
-import { checkout, polar, portal } from "@polar-sh/better-auth";
 // If your Prisma file is located elsewhere, you can change the path
 
 export const auth = betterAuth({
@@ -13,6 +13,16 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
     autoSignIn: true,
+  },
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    },
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
   },
   plugins: [
     polar({
