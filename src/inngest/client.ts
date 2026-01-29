4ceff14569dbca466926d46e610daa9e244fdea0
Matheshraju <101191899+Matheshraju4@users.noreply.github.com>
2026-01-29 14:55:09 +0530

    added Event Key

--- src/inngest/client.ts
+++ src/inngest/client.ts
@@ -4,5 +4,6 @@ import { Inngest } from "inngest";
 // Create a client to send and receive events
 export const inngest = new Inngest({
   id: "nodebase",
+  eventKey: process.env.INNGEST_EVENT_KEY,
   middleware: [realtimeMiddleware()],
 });
